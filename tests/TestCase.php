7ee52a5bf54243a901284cc5b8c141a770b92bba
Adriaan Zonnenberg <adriaanzonn@gmail.com>
2024-03-15 17:46:11 +0100

    Add support for Telescope 5

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,16 +2,9 @@
 
 namespace Tests;
 
-use AdriaanZon\TelescopeMonitor\TelescopeMonitorServiceProvider;
-use Laravel\Telescope\TelescopeServiceProvider;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            TelescopeServiceProvider::class,
-            TelescopeMonitorServiceProvider::class,
-        ];
-    }
+    use WithWorkbench;
 }
